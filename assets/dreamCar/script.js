4752dd7f1b02615ee5a2a38a8b239eb709d9c91c
Alexcious91 <radiopanetiisetso@gmail.com>
2024-03-18 15:24:29 +0200

    Fixed dream car property reveal

--- assets/dreamCar/script.js
+++ assets/dreamCar/script.js
@@ -1,15 +1,25 @@
+// const dreamCar = {
+//     make: prompt("Make:"),
+//     model: prompt("Model:"),
+//     color: prompt("Color:"),
+//     year: prompt("Year:"),
+//     price: prompt("Price in " + prompt("Currency:")),
+// };
+
 const dreamCar = {
-    make: prompt("Make:"),
-    model: prompt("Model:"),
-    color: prompt("Color:"),
-    year: prompt("Year:"),
-    price: prompt("Price in " + prompt("Currency:")),
-};
+    make: "VW",
+    model: "GOLF 6 TSI", 
+    color: "#ccc",
+    year: 2018,
+    price: "330 000"
+}
 
-document.getElementById("pricetag").innerHTML = "Price: " + dreamCar.price;
+document.getElementById("pricetag").innerHTML = `R${dreamCar.price}`;
 
-document.getElementById("model").innerHTML = "Year: " + dreamCar.year;
+document.getElementById("modelyear").innerHTML = dreamCar.year;
 
-document.body.style.backgroundColor = dreamCar.color;
+document.getElementById("body").style.backgroundColor = dreamCar.color;
 
-document.body.innerHTML = dreamCar.make + " " + dreamCar.model;
+document.body.innerHTML += dreamCar.make + " " + dreamCar.model;
+
+console.log(dreamCar.price)
